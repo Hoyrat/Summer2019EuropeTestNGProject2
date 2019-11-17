4195f9d11618f9246663505a5618d4ea2c1ef9f8
juxel <yspostbox@yahoo.com>
2019-11-17 21:54:44 +0100

    day_7_Assertions

--- src/test/java/com/cybertek/tests/Assignments/VytrackLoginTest_Negative.java
+++ src/test/java/com/cybertek/tests/Assignments/VytrackLoginTest_Negative.java
@@ -10,20 +10,14 @@ public class VytrackLoginTest_Negative {
     public static void main(String[] args) {
         WebDriver driver=WebDriverFactory.getDriver("chrome");
         driver.get("https://qa1.vytrack.com/");
-
         WebElement userInputBox=driver.findElement(By.id("prependedInput"));
-
         userInputBox.sendKeys("user7");
-
         driver.findElement(By.id("prependedInput2")).sendKeys("UserUser12");
-
         WebElement loginButton=driver.findElement(By.xpath("//button[1]"));
         loginButton.click();
-
         WebElement errorMessage=driver.findElement(By.xpath("//div[contains(text(),'Invalid user name or password.')]"));
         String actualMessage = errorMessage.getText();
         String expectedMessage = "Invalid user name or password.";
-
         if (expectedMessage.equals(actualMessage)) {
             System.out.println("PASS");
         } else {
